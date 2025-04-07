e9b33898cfc0ea27e6b0bd4ecaf379cb7619447a
JustoBellingi <jbellingi2006@gmail.com>
2025-04-07 13:19:48 -0300

    Create Ejercicio9.py

---
+++ Ejercicio9.py
@@ -0,0 +1,7 @@
+cantidadNumeros = 3
+suma = 0
+for n in range(cantidadNumeros):
+    n = int(input("Ingresa un número: "))
+    suma = suma + n
+promedio = suma / cantidadNumeros
+print("El promedio es", promedio)
